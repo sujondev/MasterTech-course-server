8882ed478cb5870a08cfa7cb5019521e911ff3d0
sujondev <emd26521@gmail.com>
2022-10-27 02:29:01 +0600

    couses json data create

--- index.js
+++ index.js
@@ -4,17 +4,24 @@ const port = process.env.port || 5000;
 const cors = require('cors')
 
 const categroies = require('./data/category.json')
+const courses = require('./data/courses.json')
 
 app.use(cors())
 
 app.get('/', (req, res) => {
-    res.send('server test successful')
+    res.send('server working')
 })
+
 
 app.get('/categroies', (req, res) => {
     res.send(categroies)
 })
 
+
+app.get('/category/:id', (req, res) => {
+    const id = (req.params.id)
+
+})
 
 
 app.listen(port, () => {

b119590ab9919afa536331979a9389dbd4e7b164
sujondev <emd26521@gmail.com>
2022-10-26 23:49:46 +0600

    api create and json data create

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const app = express();
 const port = process.env.port || 5000;
+const cors = require('cors')
+
+const categroies = require('./data/category.json')
+
+app.use(cors())
 
 app.get('/', (req, res) => {
     res.send('server test successful')
 })
 
+app.get('/categroies/:id', (req, res) => {
+    res.send(categroies)
+})
 
 
 
